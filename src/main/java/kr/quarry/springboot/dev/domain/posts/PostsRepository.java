aae870a7d27ab256eff2066d52c73fb8152c080e
jeonguho <jeonguho@gmail.com>
2021-11-16 02:01:01 +0900

    104 Page까지 커밋

---
+++ src/main/java/kr/quarry/springboot/dev/domain/posts/PostsRepository.java
@@ -0,0 +1,6 @@
+package kr.quarry.springboot.dev.domain.posts;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PostsRepository extends JpaRepository<Posts, Long> {
+}
