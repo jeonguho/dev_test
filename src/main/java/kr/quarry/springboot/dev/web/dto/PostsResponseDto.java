2e9f219b8ea8c3f062d5b98ea426f18cf5fd9f2e
jeonguho <jeonguho@gmail.com>
2021-11-18 14:39:16 +0900

    104 Page까지 커밋

---
+++ src/main/java/kr/quarry/springboot/dev/web/dto/PostsResponseDto.java
@@ -0,0 +1,21 @@
+package kr.quarry.springboot.dev.web.dto;
+
+import kr.quarry.springboot.dev.domain.posts.Posts;
+import lombok.Getter;
+
+@Getter
+public class PostsResponseDto {
+
+    private Long id;
+    private String title;
+    private String content;
+    private String author;
+
+    public PostsResponseDto(Posts entity){
+        this.id = entity.getId();
+        this.title = entity.getTitle();
+        this.content = entity.getContent();
+        this.author = entity.getAuthor();
+    }
+
+}
